012fd1a77a8ce21808c85c78f906baec5ceb69e5
thomashuan88 <thomashuan88@gmail.com>
2020-05-14 10:42:14 +0800

    2020-05-14

--- app/block.js
+++ app/block.js
@@ -1,4 +1,5 @@
-const { GENESIS_DATA } = require('./config');
+const hexToBinary = require('hex-to-binary');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
@@ -12,20 +13,21 @@ class Block {
     }
 
     static genesis() {
-        return new this (GENESIS_DATA);
+        return new this(GENESIS_DATA);
     }
 
     static mineBlock({ lastBlock, data }) {
         let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const { difficulty } = lastBlock;
+        let { difficulty } = lastBlock;
         let nonce = 0;
 
         do {
             nonce++;
+            difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
             timestamp = Date.now();
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
         
         return new this({
             timestamp,
@@ -37,7 +39,18 @@ class Block {
             // hash: cryptoHash(timestamp, lastHash, data, nonce, difficulty)
         });
     }
+
+    static adjustDifficulty({ originalBlock, timestamp }) {
+        const { difficulty } = originalBlock;
+
+        if (difficulty < 1) return 1;
+
+        if ((timestamp - originalBlock.timestamp) > MINE_RATE ) return difficulty - 1;
+
+        return difficulty + 1;
+    }    
 }
+
 
 // const block1 = new Block({
 //     timestamp: '01/01/01', 
